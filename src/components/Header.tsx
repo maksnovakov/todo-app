223280ef6505ccb71211f4793209108afea890bd
Sofia <sofia.naydenova@student.manchester.ac.uk>
2021-12-11 11:32:12 +0000

    background and header UX

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    greeting: {
+        color: '#FFD08D',
+        fontFamily: 'Roboto',
+        fontSize: '64px',
+        fontStyle: 'normal',
+        letterSpacing: '0em',
+        textAlign: 'center',
+        marginTop: '201px'
+    },
+}));
+
 interface HeaderProps {
     userName: string;
 }
 
 export default function Header(props: HeaderProps) {
+    const classes = useStyles(); 
+
     return (
-        <h1>Hey, {props.userName}</h1>
+        <div className={classes.greeting}>
+            Hey, {props.userName}
+        </div>
     )
 }
---
+++ src/data/initialTasks.tsx
@@ -0,0 +1,12 @@
+import {TaskStructure} from '../utility/types';
+
+export const INITIAL_TASKS: TaskStructure[] =  [
+    {
+        title: "UX Design",
+        completed: false
+    }, 
+    {
+        title: "Decompose UX into components", 
+        completed: false
+    }
+]
